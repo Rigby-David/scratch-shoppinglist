3a5cdd2ac9fc7bb75b700026c10f8ddac931dc6f
David <Rigby335@hotmail.com>
2022-05-19 15:29:45 -0700

    togglePurchased in fetch utils and displayListItems in list.js

--- fetch-utils.js
+++ fetch-utils.js
@@ -57,6 +57,15 @@ export async function fetchListItems() {
     }
 }
 
+export async function togglePurchased(item) {
+    const response = await client.from('shopping_list').update({ purchased: !item.purchased }).match({ id: item.id });
+
+    if (response.error) {
+        console.error(response.error.message);
+    } else {
+        return response.data;
+    }
+}
 // function checkError({ data, error }) {
 //     return error ? console.error(error) : data;
 // }
