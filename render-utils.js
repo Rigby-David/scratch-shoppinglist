b7f63e5e6cf81f2447b75462759db1efa1b6183c
David <Rigby335@hotmail.com>
2022-05-19 17:44:07 -0700

    added line through list item

--- render-utils.js
+++ render-utils.js
@@ -17,9 +17,9 @@ export function renderItem(item) {
     const div = document.createElement('div');
     div.textContent = `${item.qty} ${item.name}`;
 
-    // if (item.purchased) {
-    //     div.classList.add('complete');
-    // }
+    if (item.purchased) {
+        div.classList.add('complete');
+    }
 
     return div;
 }
